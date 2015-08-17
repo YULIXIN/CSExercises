33c402125ae352509fa236c91a9772b69d9f8558
YULIXIN <E0005379@u.nus.edu>
2015-08-17 15:31:34 +0800

    Ex09

--- CSExercises/Ex07-15/Ex09.cs
+++ CSExercises/Ex07-15/Ex09.cs
@@ -10,7 +10,11 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input a number:");
+            Double x;
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your result of the formula is: {0}", 2 * x * x - 4 * x + 3);
+
         }
     }
 }
