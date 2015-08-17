5e4ffeed41dae08db5e0af180c50e4168f51f489
YULIXIN <E0005379@u.nus.edu>
2015-08-17 11:45:51 +0800

    Ex03

--- CSExercises/Ex01-06/Ex03.cs
+++ CSExercises/Ex01-06/Ex03.cs
@@ -10,6 +10,9 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a number and i will print the square of it:");
+            Double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The square is:" + x * x);
             
         }
     }
