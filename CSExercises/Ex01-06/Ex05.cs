548c7b08eaabab8473fa5e4114750bd9be531dec
YULIXIN <E0005379@u.nus.edu>
2015-08-17 12:22:24 +0800

    Ex05

--- CSExercises/Ex01-06/Ex05.cs
+++ CSExercises/Ex01-06/Ex05.cs
@@ -9,8 +9,11 @@ namespace CSExercises
     public class Ex05
     {
         public static void Main(string[] args)
-        {
-            
+        {   
+            Console.WriteLine("Please write a number and i will print the squre root of it:");
+            Double x = Convert.ToDouble(Console.ReadLine());
+            x = Math.Sqrt(x);
+            Console.WriteLine("The squre root of it is: {0:0.000}" , x);
         }
     }
 }
