5a030be5e6e8610b7fa41f489d67afd4d76f37c4
YULIXIN <E0005379@u.nus.edu>
2015-08-17 16:15:02 +0800

    Ex12

--- CSExercises/Ex07-15/Ex12.cs
+++ CSExercises/Ex07-15/Ex12.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter the distance you have travelled:");
+            Double x = Convert.ToDouble(Console.ReadLine());
+            Double y;
+            y=2.4 + 0.4 * x;
+            y = Math.Round(y, 1);
+            Console.WriteLine("Your total fare is: ${0:0.00} ",y);
         }
     }
 }
