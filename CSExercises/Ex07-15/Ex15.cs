12ccbd6e542f344d8af478b779349825dd9fb118
YULIXIN <E0005379@u.nus.edu>
2015-08-17 17:16:15 +0800

    Ex15

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter a 3-digit integer:");
+            int w, a, b, c, d;
+            w = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(w);
+            a = w / 100;
+            b = (w % 100) / 10;
+            c = w%10;
+            int result = (a * a * a) + (b * b * b) + (c * c * c);
+            if (w == result) 
+            {
+                Console.WriteLine("Your number is an Armstrong number.");
+            }
+            else if(w != result)
+            {
+                Console.WriteLine("Your number is not an Armstrong number.");
+            }
         }
     }
 }
