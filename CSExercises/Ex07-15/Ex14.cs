6cf6f68126ef85818db28cc73113b0e21806d869
YULIXIN <E0005379@u.nus.edu>
2015-08-17 16:37:59 +0800

    Ex14

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("Dear, you need to enter the 3 sides of the triangle by the turn a,b,c:");
+            double a = System.Convert.ToDouble(Console.ReadLine());
+            double b = System.Convert.ToDouble(Console.ReadLine());
+            double c = System.Convert.ToDouble(Console.ReadLine());
+            double s = (a + b + c) / 2;
+            System.Console.WriteLine("The area of the triangle is:{0}",Math.Sqrt(s));
+
         }
     }
 }
