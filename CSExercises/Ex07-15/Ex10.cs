aa2b49bd8cf9dafe67755b0924587376c1f8c7b2
YULIXIN <E0005379@u.nus.edu>
2015-08-17 15:44:41 +0800

    Ex10

--- CSExercises/Ex07-15/Ex10.cs
+++ CSExercises/Ex07-15/Ex10.cs
@@ -10,7 +10,17 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Dear, you need to enter two points (x1,y1) and (x2,y2) by the turn x1,y1,x2,y2:");
+            Double x1, y1, x2, y2,z1,z2,z3;
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+            z1 = x2 - x1;
+            z2 = y2 - y1;
+            z3 = z1 * z1 + z2 * z2;
+            Console.WriteLine("The distance between the two points is:{0}",Math.Sqrt(z3));
+
         }
     }
 }
