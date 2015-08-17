5d0a3ead1a5bd5d66cd36225e5d7d6235292dd63
YULIXIN <E0005379@u.nus.edu>
2015-08-17 21:52:38 +0800

    Ex19

--- CSExercises/Ex16-20/Ex19.cs
+++ CSExercises/Ex16-20/Ex19.cs
@@ -1,10 +1,28 @@
+using System;
 namespace CSExercises
 {
     public class Ex19
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please show your distance by enter the kilometer you have gone:");
+            double X = Convert.ToDouble(Console.ReadLine());
+            if (X > 0 && X <= 0.5)
+            {
+                Console.WriteLine("Your cost is $2.40");
+            }
+            else if (X > 0.5 && X <= 9)
+            {
+                Console.WriteLine("Your cost is $ {0:0.00}", 0.4 * (X - 0.5)+2.4);
+            }
+            else if (X > 9)
+            {
+                Console.WriteLine("Your cost is $ {0:0.00}", 0.05 * (X - 9) + 5.8);
+            }
+            else if (X <= 0)
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
