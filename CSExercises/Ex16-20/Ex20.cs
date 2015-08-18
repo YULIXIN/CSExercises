6f7cffbcaaa0a250de2c15cc8207593537fe2ea0
YULIXIN <E0005379@u.nus.edu>
2015-08-18 14:13:32 +0800

    Ex20

--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -1,10 +1,44 @@
-﻿namespace CSExercises
+﻿using System;
+namespace CSExercises
 {
     public class Ex20
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter quantity for TV");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter quantity for DVD");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter quantity for MP3");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int Y;
+            Y = 900 * a + 500 * b + 700 * c;
+            if (Y < 5000)
+            {
+                Console.WriteLine("Total price for this order is :$" + Y);
+            }
+            else if (Y >= 5000 && Y < 10000)
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("Total price for this order is :$" + 0.9 * Y);
+                }
+                else
+                {
+                    Console.WriteLine("Total price for this order is :$" + Y);
+                }
+            }
+            else if (Y >= 10000)
+            {
+                if (c == 0)
+                {
+                    Console.WriteLine("Total price for this order is :$" + 0.85 * Y);
+                }
+                else
+                {
+                    Console.WriteLine("Total price for this order is :$" + Y);
+                }
+            }
         }
     }
 }
